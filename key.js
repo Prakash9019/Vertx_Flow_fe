6aac098306a8fbf28de92ccef82a49f61bc88170
Prakash9019 <plsprakash2003@gmail.com>
2026-03-17 16:20:10 +0530

    added

--- key.js
+++ key.js
@@ -1,4 +1,4 @@
-const API_KEY = "https://vertx-flow-server-817384216349.us-central1.run.app";
+const API_KEY = "https://vertx-flow-server.vercel.app";
 export const API_URL = "https://reach-backend-817384216349.europe-west1.run.app";
 export default API_KEY;
 
