7d612c5fc17ec9acd951808eb85d26c2d84b261d
Prakash9019 <plsprakash2003@gmail.com>
2026-03-18 12:51:45 +0530

    added

--- key.js
+++ key.js
@@ -1,10 +1,10 @@
-const API_KEY = "https://vertx-flow-server.vercel.app";
+const API_KEY = "https://vertx-flow-server.onrender.com";
 export const API_URL = "https://reach-backend-817384216349.europe-west1.run.app";
 export default API_KEY;
 
 // https://auth-flow.govertx.com/
 //  https://reach-backend-817384216349.europe-west1.run.app/
 
-
+// https://vertx-flow-server.onrender.com/
 
 // http://localhost:5000 
